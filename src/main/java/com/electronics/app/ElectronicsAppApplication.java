7c70b411c154dbdb272617495952a8166f025b9d
HP <HP@DESKTOP-0SN7540>
2021-09-09 19:17:49 +0200

    user privileges

---
+++ src/main/java/com/electronics/app/ElectronicsAppApplication.java
@@ -0,0 +1,13 @@
+package com.electronics.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electronics/app/ElectronicsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronics.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
